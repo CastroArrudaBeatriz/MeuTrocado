494c1e2fdb55192477fdd8c49c19bf1a8ff70e61
Beatriz Castro <beatriz_arruda_castro@hotmail.com>
2018-06-14 00:57:11 -0300

    Adicionando componente navbar, simulado e ajustando as views do painel e grafico

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { PainelComponent } from './painel/painel.component';
 import { GraficoComponent } from './grafico/grafico.component';
+import { SimuladorComponent } from './simulador/simulador.component';
 
 
 const APP_ROUTES: Routes =[
     {path: '', component: PainelComponent},
     {path: 'grafico', component: GraficoComponent},
+    {path: 'simulador', component: SimuladorComponent},
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(APP_ROUTES);
